05b97ccf907a3462f0e706a849714f041b8c5968
DreVinciBot <35714752+DreVinciBot@users.noreply.github.com>
2020-12-31 11:30:02 -0500

    added project to github

---
+++ Robot Gaze/Assets/AgentController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentController : MonoBehaviour
+{
+    public GameObject robot;
+    public GameObject player;
+    public GameObject DestinationPosition;
+    public NavMeshAgent agent;
+    private Vector3 StartingPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartingPosition = robot.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(DestinationPosition.transform.position);
+
+        float Distance_robot_goal = Vector3.Distance(robot.transform.position, DestinationPosition.transform.position);
+        float Distance_robot_player = Vector3.Distance(robot.transform.position, player.transform.position);
+ 
+        if(Distance_robot_goal < 0.1)
+        {
+            robot.transform.position = StartingPosition;
+        }
+    }
+}
---
+++ Robot Gaze/Assets/MaterialSwapper.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialSwapper : MonoBehaviour
+{
+    public GameObject robot;
+    public GameObject player;
+    public float robot_signal = 4.0f;
+    private bool robot_signal_check;
+
+    public Material[] allMaterials = new Material[3];
+    Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        robot_signal_check = false;
+
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = allMaterials[0];
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Distance_robot_player = Vector3.Distance(robot.transform.position, player.transform.position);
+
+        if (Distance_robot_player < robot_signal && !robot_signal_check)
+        {
+            MaterialSwap();
+            robot_signal_check = true;
+        }
+
+        if (Distance_robot_player > robot_signal && robot_signal_check)
+        {
+            MaterialSwap();
+            robot_signal_check = false;
+        }
+    }
+
+    void MaterialSwap()
+    {
+        if (rend.sharedMaterial == allMaterials[1])
+        {
+            rend.sharedMaterial = allMaterials[0];
+        }
+
+        else if (rend.sharedMaterial == allMaterials[0])
+        {
+            rend.sharedMaterial = allMaterials[1];
+        }
+    }
+}
